78722687be282d395d2758f5799682f8fdf8dccc
Ruhlenko <ruhlenko@gmail.com>
2015-06-16 19:59:14 +0500

    Added ini file support

---
+++ IniFile.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace hcGate
+{
+    class IniFile
+    {
+        private static const int _bufferSize = 255;
+
+        public string _file;
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileString(
+            string section, string key,
+            string defaultValue, StringBuilder returnValue,
+            int size, string fileName);
+
+        public IniFile(string iniFile)
+        {
+            _file = iniFile;
+        }
+
+        public string ReadValue(string section, string key, string defaultValue)
+        {
+            StringBuilder returnValue = new StringBuilder(_bufferSize);
+            GetPrivateProfileString(section, key, defaultValue, returnValue, _bufferSize, _file);
+            return returnValue.ToString();
+        }
+    }
+}
