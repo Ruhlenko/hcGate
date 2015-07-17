7d0145ab5a5dbd68b9baa0a465ab6f9ae9735002
Ruhlenko <ruhlenko@gmail.com>
2015-07-17 15:19:56 +0500

    Faster HexParser

--- HexParser.cs
+++ HexParser.cs
@@ -6,61 +6,69 @@ namespace hcGate
 {
     static class HexParser
     {
-
         public static Byte GetByte(byte[] buffer, int index)
         {
-            Byte data = 0;
-            try
-            {
-                data = Byte.Parse(Encoding.ASCII.GetString(buffer, index, sizeof(Byte) * 2), NumberStyles.HexNumber);
-            }
-            catch { }
+            Byte data;
+
+            Byte.TryParse(
+                Encoding.ASCII.GetString(buffer, index, sizeof(Byte) * 2),
+                NumberStyles.HexNumber,
+                CultureInfo.InvariantCulture,
+                out data);
+
             return data;
         }
 
         public static Int16 GetInt16(byte[] buffer, int index)
         {
-            Int16 data = 0;
-            try
-            {
-                data = Int16.Parse(Encoding.ASCII.GetString(buffer, index, sizeof(Int16) * 2), NumberStyles.HexNumber);
-            }
-            catch { }
+            Int16 data;
+
+            Int16.TryParse(
+                Encoding.ASCII.GetString(buffer, index, sizeof(Int16) * 2),
+                NumberStyles.HexNumber,
+                CultureInfo.InvariantCulture,
+                out data);
+
             return data;
         }
 
         public static UInt16 GetUInt16(byte[] buffer, int index)
         {
-            UInt16 data = 0;
-            try
-            {
-                data = UInt16.Parse(Encoding.ASCII.GetString(buffer, index, sizeof(UInt16) * 2), NumberStyles.HexNumber);
-            }
-            catch { }
+            UInt16 data;
+
+            UInt16.TryParse(
+                Encoding.ASCII.GetString(buffer, index, sizeof(UInt16) * 2),
+                NumberStyles.HexNumber,
+                CultureInfo.InvariantCulture,
+                out data);
+
             return data;
         }
 
         public static Int32 GetInt32(byte[] buffer, int index)
         {
-            Int32 data = 0;
-            try
-            {
-                data = Int32.Parse(Encoding.ASCII.GetString(buffer, index, sizeof(Int32) * 2), NumberStyles.HexNumber);
-            }
-            catch { }
+            Int32 data;
+
+            Int32.TryParse(
+                Encoding.ASCII.GetString(buffer, index, sizeof(Int32) * 2),
+                NumberStyles.HexNumber,
+                CultureInfo.InvariantCulture,
+                out data);
+
             return data;
         }
 
         public static UInt32 GetUInt32(byte[] buffer, int index)
         {
-            UInt32 data = 0;
-            try
-            {
-                data = UInt32.Parse(Encoding.ASCII.GetString(buffer, index, sizeof(UInt32) * 2), NumberStyles.HexNumber);
-            }
-            catch { }
+            UInt32 data;
+
+            UInt32.TryParse(
+                Encoding.ASCII.GetString(buffer, index, sizeof(UInt32) * 2),
+                NumberStyles.HexNumber,
+                CultureInfo.InvariantCulture,
+                out data);
+
             return data;
         }
-
     }
 }
